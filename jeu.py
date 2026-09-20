3368e482585f9b3dc3162a8983accd8c4ef5dd26
Moutalib_Dev'x <guiramoutalib@gmail.com>
2026-09-20 23:43:39 +0000

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -4,16 +4,20 @@ from fonctions import*
 
 user=recup_nom_utilisateur()
 scores=recup_score()
-if user not in scores.keys():
-    scores[user] = 0
+if user not in scores . keys () :
+    scores [user] = 0
+else :
+    print("vous avez deja {0} points".format(scores.get(user)))
 cont="o"
+score=0
 while cont=="o":
-    print("Joueur {0}: {1} point(s)".format(user, scores[user]))
+    
+    print("Joueur {0}".format(user))
     nb_chances =tent
     mot=choisir_mots()
-    nb_chances=verificateur(mot)
-    scores[user] += nb_chances  
+    score+=verificateur(mot)
+    scores [ user ] += score
     cont = input("Souhaitez-vous continuer la partie (O/N) ?")
     cont=cont.lower()
-save_score()
-print("Vous finissez la partie avec {0} points .Votre score tottal est de :{1}".format(nb_chances,scores[user]))
+print ( " Vous finissez la partie avec {0} points .cumul ={1}  point " . format ( score ,scores[user]))
+save_score(scores)
